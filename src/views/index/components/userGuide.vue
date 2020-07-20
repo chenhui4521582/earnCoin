36e0c714770e2b873d683e83ce0eeb4b20aa1388
chenhuihui <461822621@qq.com>
2020-07-20 11:01:26 +0800

    积分墙修改

---
+++ src/views/index/components/userGuide.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user-guide">
+    <div class="step1" v-if="step == 1">
+      <div class="cover"></div>
+    </div>
+    <div class="step2" v-if="step == 2"></div>
+    <div class="step3" v-if="step == 3"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'userGuide',
+  data: () => ({
+    step: 1
+  })  
+}
+</script>
+<style lang="less" scoped>
+
+</style>
