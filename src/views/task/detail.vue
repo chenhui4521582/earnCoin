148dea22c9d08c0a72cd889c81224446b7c18641
chenhuihui <461822621@qq.com>
2020-08-27 19:06:43 +0800

    app 界面优化

--- src/views/task/detail.vue
+++ src/views/task/detail.vue
@@ -699,7 +699,6 @@ export default {
           width: .7rem;
           line-height: 1.2rem;
           text-align: center;
-          color: #DCDCDC;
         }
         .desc {
           display: flex;
