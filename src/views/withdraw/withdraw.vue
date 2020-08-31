fdc7c912bad0f82eb6d7763bfd6f3410b959b891
chenhuihui <461822621@qq.com>
2020-08-31 15:35:35 +0800

    修改

--- src/views/withdraw/withdraw.vue
+++ src/views/withdraw/withdraw.vue
@@ -65,7 +65,8 @@
       </div>
     </div>
     <div class="withdraw-btn" @click="_userWithDraw">
-      <div v-if="!canWithdraw" class="btn disable">金币不足</div>
+      <div v-if="!isBindWechat" class="btn disable">请绑定微信</div>
+      <div v-else-if="!canWithdraw" class="btn disable">金币不足</div>
       <div v-else class="btn" :class="{'disable': !canWithdraw}">立即提现</div>
     </div>
     <!-- rule 弹框 -->
