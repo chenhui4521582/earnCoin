0c0131a381728d9072e8f1cb9a0178b0f3843b27
chenhuihui <461822621@qq.com>
2020-09-25 14:18:44 +0800

    按钮修改

--- src/utils/native/index.js
+++ src/utils/native/index.js
@@ -376,6 +376,22 @@ AppCall.extend({
   downloadApk: function (url) {
     return this.call('downloadApk', url)
   },
+  // 查看手机上是否下载了app 传文件名
+  checkIsDownload: function (fileName) {
+    return AppCall.call("checkIsDownload", fileName)
+  },
+  // 查看手机上是否安装了app 传包名
+  checkIsInstall: function (packageName) {
+    return AppCall.call("checkIsInstall", packageName)
+  },
+  // 安装app  传文件名
+  installApk: function (fileName) {
+    return AppCall.call("installApk", fileName)
+  },
+  // 打开APP 传包名
+  openPackage: function (packageName) {
+    return AppCall.call("openPackage", packageName)
+  },
   // APP本地存放数据
   setData: function (key, value) {
     AppCall.call("setData", key, value)
